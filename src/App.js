57b61bc62e6b6b739828f94efa2bea404172ba66
Mike Digger <digger.misko@gmail.com>
2022-06-15 12:32:28 +0300

    without batch associations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import sendToHubspot from './services/hubspot';
+
+const App = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [message, setMessage] = useState('');
+    const [company, setCompany] = useState('');
+    const [website, setWebsite] = useState('');
+
+    const controlName = (e) => setName(e.target.value);
+    const controlEmail = (e) => setEmail(e.target.value);
+    const controlPhone = (e) => setPhone(e.target.value);
+    const controlMessage = (e) => setMessage(e.target.value);
+    const controlWebsite = (e) => setWebsite(e.target.value);
+    const controlCompany = (e) => setCompany(e.target.value);
+
+    const newUser = {
+        firstname: name,
+        email,
+        mobilephone: phone,
+        message,
+        company,
+        website
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        sendToHubspot(newUser)}
+
+    // прибрати атрибут name з input
+    return (
+        <form
+            onSubmit={(e) => onFormSubmit(e)}>
+            <input
+                onChange={controlName}
+                value={name}
+                placeholder="name"
+                type="text"
+                name="name" />
+            <input
+                onChange={controlEmail}
+                value={email}
+                placeholder="email"
+                type="email"
+                name="email"/>
+            <input
+                onChange={controlPhone}
+                value={phone}
+                placeholder="phone"
+                type="tel"
+                name="mobilephone"
+                id="" />
+            <input
+                onChange={controlMessage}
+                value={message}
+                placeholder="message"
+                type="text"
+                name="message"/>
+            <input
+                onChange={controlCompany}
+                value={company}
+                placeholder="company name"
+                type="text"
+                name="company_name"/>
+            <input
+                onChange={controlWebsite}
+                value={website}
+                placeholder="company link"
+                type="url"
+                name="website"/>
+            <button type="submit">submit</button>
+        </form>
+    )
 }
 
 export default App;
---
+++ src/services/hubspot.js
@@ -0,0 +1,121 @@
+const sendToHubspot = (newContact) => {
+
+    const proxyURL = 'https://http-cors-proxy.p.rapidapi.com/';
+    const hapiKey = 'eu1-ea79-cb21-4d49-9f86-da6cde4075e2';
+
+    const getRequestHeaders = () => {
+        return {
+            'content-type': 'application/json',
+            // stuff for http-cors-proxy
+            origin: 'http://localhost:3000',
+            'x-requested-with': 'http://localhost:3000',
+            'X-RapidAPI-Key': '248e0c5260msh354a5b43e6f14f9p13b870jsn926da4ac7752',
+            'X-RapidAPI-Host': 'http-cors-proxy.p.rapidapi.com'
+        }
+    }
+
+    const getHubspotObjectURL = (object) => {
+        return `https://api.hubapi.com/crm/v3/objects/${object}?hapikey=${hapiKey}`
+    }
+
+    const getHubspotAssocURL = (params) => {
+        const {object, objectId, toObjectType, toObjectId, associationType} = params;
+        return `https://api.hubapi.com/crm/v3/objects/${object}/${objectId}/associations/${toObjectType}/${toObjectId}/${associationType}?hapikey=${hapiKey}`
+    }
+
+    const newDeal = (newContact) => {
+        return {
+            "dealname": `RR Site Enquiry: ${newContact.company}`,
+            "dealstage": "appointmentscheduled",
+        }
+    }
+
+    const newCompany = (newContact) => {
+        return {
+            "name": newContact.company,
+            "website": newContact.website
+        }
+    }
+
+    const createHubspotObject = async (object, props) => {
+        const res = await fetch(`${proxyURL}${getHubspotObjectURL(object)}`, {
+            method: 'POST',
+            headers: getRequestHeaders(),
+            body: JSON.stringify({ properties: props })
+        });
+        return await res.json()
+    }
+
+    const createAssociation = (params) => {
+        fetch(`${proxyURL}${getHubspotAssocURL(params)}`, {
+            method: 'PUT',
+            headers: getRequestHeaders()
+        })
+    }
+
+    const createContact = async () => {
+        return await createHubspotObject('contacts', newContact)
+    }
+
+    const createDeal = async () => {
+        return await createHubspotObject('deals', newDeal(newContact))
+    }
+
+    const createCompany = async () => {
+        return await createHubspotObject('companies', newCompany(newContact))
+    }
+
+    const createNote = async () => {
+        const {firstname, company, email, website, mobilephone, message} = newContact
+        const res = await fetch(`${proxyURL}${getHubspotObjectURL('notes')}`, {
+            method: 'POST',
+            headers: getRequestHeaders(),
+            body: JSON.stringify({
+                properties: {
+                    "hs_timestamp": new Date(),
+                    "hs_note_body": 
+                        `Name: ${firstname}, <br />
+                        Company: ${company}, <br />
+                        Email: ${email}, <br />
+                        Website: ${website}, <br />
+                        Telephone: ${mobilephone}, <br />
+                        Message: ${message}, <br />`,
+                    
+                }
+            })
+        });
+        
+        return await res.json();
+    }
+
+    createContact()
+        .then(contact => {
+            createCompany()
+                .then(company => createAssociation({
+                    object: 'companies',
+                    objectId: company.id,
+                    toObjectType: 'contacts',
+                    toObjectId: contact.id,
+                    associationType: 'company_to_contact'
+                }))
+            createDeal()
+                .then(deal => createAssociation({
+                    object: 'deals',
+                    objectId: deal.id,
+                    toObjectType: 'contacts',
+                    toObjectId: contact.id,
+                    associationType: 'deal_to_contact'
+                }))
+            createNote()
+                .then(note => createAssociation({
+                    object: 'notes',
+                    objectId: note.id,
+                    toObjectType: 'contacts',
+                    toObjectId: contact.id,
+                    associationType: 'note_to_contact'
+                }))
+        })
+
+}
+
+export default sendToHubspot;
